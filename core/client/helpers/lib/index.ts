f5eab13bbc781c9c7b58d029d78db394cc3f083c
Kirill Kuznetsov <kka.fullstack@gmail.com>
2026-04-28 10:35:32 +0300

    fix helper

--- core/client/helpers/lib/index.ts
+++ core/client/helpers/lib/index.ts
@@ -1,6 +1,5 @@
 export * from "./delay";
 export * from "./draw-text-3d";
-export * from "./initialize";
 export * from "./prepare-instructions-scaleform";
 export * from "./random";
 export * from "./register-state-bag";
